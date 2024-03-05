18bd5319071b01832e72086e3bb66d3001455cd8
Caio Diamantino <caiodiamantino53@gmail.com>
2024-03-05 08:09:23 -0300

    ap01(git-python) menu somar implementada

---
+++ menu.py
@@ -0,0 +1,19 @@
+import calculadora
+
+def menu():
+    print("1. Soma")
+    print("2. Subtração")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("0. Sair")
+    seleção = input(int("Insira a opção desejada"))
+
+    if seleção == 1:
+        a = input(int("Insira o primeiro numero"))
+        b = input(int("Insira o segundo numero"))
+        soma = calculadora.somar(a, b)
+        print(f'{a} + {b} = {soma}')
+    
+    return
+
+menu()
